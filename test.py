0590a9b890351f67eaa3a690ace3e66bcad21a38
sheiun <me.sheiun@gmail.com>
2020-10-24 23:39:23 +0800

    Add parse test

--- test.py
+++ test.py
@@ -11,3 +11,8 @@ class Test(TestCase):
     def test_base64(self):
         dice = Dice("bzz").roll(base64=True)
         self.assertIn("base64", dice)
+
+    def test_parse(self):
+        dices = Dice.parse("(dice20)  (dice8)")
+        self.assertTrue(20 >= dices[0]["result"] >= 1)
+        self.assertTrue(8 >= dices[1]["result"] >= 1)
