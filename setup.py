59831bd125842ec3317250e3cbc55a1645f8e841
sheiun <me.sheiun@gmail.com>
2020-10-24 23:50:20 +0800

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="plurk-dice",
-    version="0.0.2",
+    version="0.0.3",
     author="SheiUn",
     author_email="develop@sheiun.me",
     description="A third-party Python API of Plurk Dice Emojis",
